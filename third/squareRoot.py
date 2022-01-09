ba6f36f1f4e858bd649c7dfced8f9d7daa7acf9b
hndrr <yozhik20@gmail.com>
2022-01-09 18:55:46 +0900

    平方根

---
+++ third/squareRoot.py
@@ -0,0 +1,27 @@
+from random import randint
+
+
+def average(a, b):
+    return (a + b) / 2
+
+
+def squareRoot(num, low, high):
+    # lowからhigまでの範囲内で
+    # 数当てゲームの戦略を利用することにより
+    # 指定された数字の平方根を見つける
+
+    for i in range(20):
+        guess = average(low, high)
+
+        if guess ** 2 == num:
+            break
+        elif guess ** 2 > num:
+            # print("違います。もっと小さいです")
+            high = guess
+        else:
+            # print("違います。もっと大きいです")
+            low = guess
+    print(guess)
+
+
+squareRoot(60, 7, 8)
